49f426b94d38e1c8f657b23f7ee8653743b29969
Mat Brewer <mat@m3-design.co.uk>
2023-11-07 12:08:57 +0700

    Homepage v2

--- main.js
+++ main.js
@@ -676,6 +676,648 @@ function postImageGridParallax() {
   }
 }
 
+function millionQuestions() {
+  var rectangleData = [
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e3407634605830cf0b110_question-01.png',
+      width: 366,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e3408c6cb4ae37a8c2668_question-02.png',
+      width: 312,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340a5665c60c90870223_question-03.png',
+      width: 316,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e34089a6a15235f43da25_question-04.png',
+      width: 356,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e3408532c272ff41f4017_question-05.png',
+      width: 146,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340a177a0e346ceea423_question-06.png',
+      width: 344,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e3409df2f00e1ba2bf1f4_question-07.png',
+      width: 242,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340ac2584cf0a115a646_question-08.png',
+      width: 372,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e3409c2584cf0a115a5cc_question-09.png',
+      width: 358,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340a859f7ef17cd2e904_question-10.png',
+      width: 236,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e3409b1ba3e6fb1d869e8_question-11.png',
+      width: 272,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340af83764e0d73cf179_question-12.png',
+      width: 360,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340a29ef510647761b13_question-13.png',
+      width: 280,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340adf2f00e1ba2bf267_question-14.png',
+      width: 326,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340aec0aa957abec7380_question-15.png',
+      width: 486,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340b29ef510647761bbd_question-16.png',
+      width: 436,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340be0d72d521ae60002_question-17.png',
+      width: 418,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340b1e95df1c73fb6c84_question-18.png',
+      width: 344,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340b172747b22453bc57_question-19.png',
+      width: 228,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340bfff61bfa43c900d3_question-20.png',
+      width: 426,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340b172747b22453bdfa_question-21.png',
+      width: 278,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340b621cc586473fc55a_question-22.png',
+      width: 360,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340cda9971e1c3e80bbb_question-23.png',
+      width: 430,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340b08d71e9b0a053cdb_question-24.png',
+      width: 224,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340c6fc80825a1a47c97_question-25.png',
+      width: 404,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340cb1ba3e6fb1d86ca1_question-26.png',
+      width: 502,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340bcc51dbe5e0063a61_question-27.png',
+      width: 446,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340c634605830cf0b871_question-28.png',
+      width: 494,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340ce0d72d521ae60195_question-29.png',
+      width: 464,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340cc81e99686b8f982c_question-30.png',
+      width: 336,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340c547361b498a7c7ff_question-31.png',
+      width: 342,
+      height: 76
+    },
+    {
+      texture: 'https://uploads-ssl.webflow.com/64b36a0c61b7437799a31357/652e340cda9971e1c3e80c07_question-32.png',
+      width: 290,
+      height: 76
+    }
+  ];
+
+  var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Composites = Matter.Composites,
+    Common = Matter.Common,
+    MouseConstraint = Matter.MouseConstraint,
+    Mouse = Matter.Mouse,
+    Composite = Matter.Composite,
+    Bodies = Matter.Bodies;
+
+  // Create engine
+  var engine = Engine.create(),
+    world = engine.world;
+
+  // Get the "questionsContainer" element
+  var questionsContainer = document.getElementById('questionsContainer');
+
+  // Function to set canvas dimensions based on "questionsContainer"
+  function setCanvasDimensions() {
+    var width = questionsContainer.clientWidth;
+    var height = questionsContainer.clientHeight;
+
+    render.options.width = width;
+    render.options.height = height;
+
+    // Calculate the number of rectangles based on the viewport width
+    var numRectangles = Math.floor(width / 24); // Adjust the factor as needed
+    generateRectangles(numRectangles);
+  }
+
+  // Function to generate rectangles
+  function generateRectangles(numRectangles) {
+    Composite.clear(world);
+    var objects = [];
+    var totalRectangles = 0;
+    var dataIndex = 0;
+
+    while (totalRectangles < numRectangles) {
+      var x = Common.random(20, render.canvas.width - 20);
+      var y = Common.random(20, render.canvas.height - 20);
+      var data = rectangleData[dataIndex % rectangleData.length]; // Cycle through rectangleData
+
+      var rect = Bodies.rectangle(x, y, data.width, data.height, {
+        chamfer: { radius: 32 }, // Border radius
+        render: {
+          sprite: {
+            texture: data.texture
+          }
+        }
+      });
+
+      objects.push(rect);
+      totalRectangles++;
+      dataIndex++;
+    }
+
+    Composite.add(world, objects);
+  }
+
+  // Create renderer with dynamic width and height
+  var render = Render.create({
+    element: questionsContainer, // Use the "questionsContainer" div
+    engine: engine,
+    options: {
+      width: questionsContainer.clientWidth, // Initial width
+      height: questionsContainer.clientHeight, // Initial height
+      pixelRatio: 2,
+      background: 'rgba(0, 0, 0, 0)',
+      // showAngleIndicator: true,
+      wireframes: false
+      // isStatic: true
+    }
+  });
+
+  Render.run(render);
+
+  // Create runner
+  var runner = Runner.create();
+  Runner.run(runner, engine);
+
+  // Set the runner to start in a paused state
+  runner.enabled = false;
+
+  // Set canvas dimensions initially
+  setCanvasDimensions();
+
+  Composite.add(world, [
+    // walls
+    Bodies.rectangle(render.canvas.width / 2, 0, render.canvas.width, 1, {
+      isStatic: true,
+      render: {
+        fillStyle: 'transparent',
+        strokeStyle: 'transparent'
+      }
+    }),
+    Bodies.rectangle(render.canvas.width / 2, render.canvas.height - 16, render.canvas.width, 1, {
+      isStatic: true,
+      render: {
+        fillStyle: 'transparent',
+        strokeStyle: 'transparent'
+      }
+    }),
+    Bodies.rectangle(render.canvas.width, render.canvas.height / 2, 1, render.canvas.height, {
+      isStatic: true,
+      render: {
+        fillStyle: 'transparent',
+        strokeStyle: 'transparent'
+      }
+    }),
+    Bodies.rectangle(0, render.canvas.height / 2, 1, render.canvas.height, {
+      isStatic: true,
+      render: {
+        fillStyle: 'transparent',
+        strokeStyle: 'transparent'
+      }
+    })
+  ]);
+
+  // Add mouse control
+  var mouse = Mouse.create(render.canvas),
+    mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        stiffness: 0.2,
+        render: {
+          visible: false
+        }
+      }
+    });
+
+  Composite.add(world, mouseConstraint);
+
+  // Keep the mouse in sync with rendering
+  render.mouse = mouse;
+
+  mouseConstraint.mouse.element.removeEventListener('mousewheel', mouseConstraint.mouse.mousewheel);
+  mouseConstraint.mouse.element.removeEventListener('DOMMouseScroll', mouseConstraint.mouse.mousewheel);
+
+  // Handle window resize events
+  // window.addEventListener("resize", setCanvasDimensions);
+
+  // Fit the render viewport to the scene
+  Render.lookAt(render, {
+    min: { x: 0, y: 0 },
+    max: { x: render.canvas.width, y: render.canvas.height }
+  });
+
+  // let matterEnabled = false; // Flag to control Matter.js engine
+
+  const enableMatter = () => {
+    runner.enabled = true; // Enable the Matter.js engine
+  };
+
+  const disableMatter = () => {
+    runner.enabled = false; // Disable the Matter.js engine
+  };
+
+  // Add scrollTrigger
+  ScrollTrigger.create({
+    trigger: '#questionsContainer', // Element that triggers the animation
+    start: 'top 80%',
+    end: 'bottom top',
+    onEnter: enableMatter,
+    onLeave: disableMatter,
+    onEnterBack: enableMatter
+  });
+}
+
+function showVideoAnimation() {
+  if (document.querySelector('.show-hero__kw')) {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.show-hero__kw',
+        start: 'top 115%',
+        end: 'top 10%',
+        scrub: true,
+        pin: false,
+        pinSpacing: false
+      }
+    });
+
+    tl.set('.show-hero-container__kw', {
+      scale: 0.4,
+      transformOrigin: 'center center'
+    });
+    tl.to('.show-hero-container__kw', {
+      scale: 1,
+      ease: 'power1.out'
+    });
+  }
+}
+
+function bgVideoPlayPause() {
+  if (document.querySelector('.show-hero__kw')) {
+    let mm = gsap.matchMedia();
+
+    mm.add('(max-width: 767px)', () => {
+      // mobile setup code here...
+      const video = document.getElementById('bgVideoMobile');
+
+      function playVideo() {
+        video.play();
+        console.log('Play video');
+      }
+
+      function pauseVideo() {
+        video.pause();
+        console.log('Pause video');
+      }
+
+      ScrollTrigger.create({
+        trigger: '.show-hero__kw',
+        start: 'top bottom',
+        end: 'bottom top',
+        onEnter: playVideo,
+        onLeave: pauseVideo,
+        onEnterBack: playVideo,
+        onLeaveBack: pauseVideo
+      });
+    });
+
+    mm.add('(min-width: 768px)', () => {
+      // desktop setup code here...
+      const video = document.getElementById('bgVideo');
+
+      function playVideo() {
+        video.play();
+        console.log('Play video');
+      }
+
+      function pauseVideo() {
+        video.pause();
+        console.log('Pause video');
+      }
+
+      ScrollTrigger.create({
+        trigger: '.show-hero__kw',
+        start: 'top bottom',
+        end: 'bottom top',
+        onEnter: playVideo,
+        onLeave: pauseVideo,
+        onEnterBack: playVideo,
+        onLeaveBack: pauseVideo
+      });
+    });
+  }
+}
+
+function splitTextChars() {
+  if (document.querySelector('[data-splitText="chars"]')) {
+    const textEl = document.querySelector('[data-splitText="chars"]');
+    const splitText = new SplitType(textEl, {
+      types: 'words, chars',
+      tagName: 'span'
+    });
+
+    let tl = gsap.timeline({
+      // repeat: -1,
+      // repeatDelay: 0.5,
+      // paused: false
+    });
+
+    gsap.set(splitText.words, { overflow: 'hidden' });
+
+    tl.from(splitText.chars, {
+      yPercent: 100,
+      autoAlpha: 0,
+      duration: 0.25,
+      // ease: "power1.in",
+      stagger: {
+        // each: 0.025,
+        amount: 0.75, // total amount
+        ease: 'power1.in'
+      }
+    });
+    return tl;
+  }
+}
+
+function splitTextLines() {
+  if (document.querySelector('[data-splitText="lines"]')) {
+    const textElements = document.querySelectorAll('[data-splitText="lines"]');
+    const timelines = [];
+
+    textElements.forEach((textEl) => {
+      const splitText = new SplitType(textEl, {
+        types: 'lines, words',
+        tagName: 'span'
+      });
+
+      let tl = gsap.timeline({
+        // repeat: -1,
+        // repeatDelay: 0.5,
+        // paused: false
+      });
+
+      gsap.set(splitText.lines, { overflow: 'hidden' });
+
+      tl.from(splitText.lines, {
+        yPercent: 100,
+        autoAlpha: 0,
+        duration: 0.3,
+        stagger: {
+          each: 0.075,
+          // amount: 0.7, // total amount
+          ease: 'none'
+        }
+      });
+
+      timelines.push(tl);
+    });
+
+    return timelines; // Return an array of timelines
+  }
+}
+
+function masterTimeline() {
+  // stitch them together in a master timeline...
+  const individualTimelines = splitTextLines();
+
+  var master = gsap.timeline({
+    // repeat: -1,
+    // repeatDelay: 3,
+    paused: true
+  });
+
+  master.add(splitTextChars());
+  individualTimelines.forEach((timeline) => {
+    master.add(timeline, 0.5);
+  });
+
+  ScrollTrigger.create({
+    trigger: '.whatwedo__heading',
+    start: 'top 80%',
+    onEnter: () => {
+      // Play the master timeline when the trigger element is in view
+      master.play();
+    }
+  });
+}
+
+function inspirationHeroCardHover() {
+  let inspoCards = document.querySelectorAll('.inspiration-card__link');
+
+  inspoCards.forEach((inspoCard) => {
+    let anim = gsap.to(inspoCard.querySelector('.inspiration-card__content'), {
+      paused: true,
+      ease: 'power1.out',
+      duration: 0.4,
+      y: -128
+    });
+
+    inspoCard.addEventListener('mouseenter', () => anim.play());
+    inspoCard.addEventListener('mouseleave', () => anim.reverse());
+  });
+}
+
+function homeCardCarousel() {
+  // Initiate swipers and default settings
+  $('.slider-main_home').each(function (index) {
+    let loopMode = false;
+    if ($(this).attr('loop-mode') === 'true') {
+      loopMode = true;
+    }
+    let sliderDuration = 260;
+    if ($(this).attr('slider-duration') !== undefined) {
+      sliderDuration = +$(this).attr('slider-duration');
+    }
+    const swiper = new Swiper($(this).find('.swiper')[0], {
+      speed: sliderDuration,
+      loop: loopMode,
+      autoHeight: false,
+      centeredSlides: loopMode,
+      followFinger: true,
+      freeMode: false,
+      slideToClickedSlide: false,
+      slidesPerView: 1.2,
+      spaceBetween: 16,
+      rewind: false,
+      mousewheel: {
+        forceToAxis: true
+      },
+      keyboard: false,
+      breakpoints: {
+        // mobile landscape
+        480: {
+          slidesPerView: 1.5,
+          spaceBetween: 16
+        },
+        // tablet
+        768: {
+          slidesPerView: 2.2,
+          spaceBetween: 16
+        },
+        // desktop
+        992: {
+          slidesPerView: 3.1,
+          spaceBetween: 16
+        },
+        1280: {
+          slidesPerView: 4.1,
+          spaceBetween: 16
+        }
+      },
+      pagination: false,
+      navigation: {
+        nextEl: $(this).find('.swiper-next')[0],
+        prevEl: $(this).find('.swiper-prev')[0],
+        disabledClass: 'is-disabled'
+      },
+      scrollbar: false,
+      slideActiveClass: 'is-active',
+      slideDuplicateActiveClass: 'is-active'
+    });
+  });
+}
+
+function growingmindsCarousel() {
+  // Initiate swipers and default settings
+  $('.slider-main__growingminds').each(function (index) {
+    let loopMode = false;
+    if ($(this).attr('loop-mode') === 'true') {
+      loopMode = true;
+    }
+    let sliderDuration = 260;
+    if ($(this).attr('slider-duration') !== undefined) {
+      sliderDuration = +$(this).attr('slider-duration');
+    }
+    const swiper = new Swiper($(this).find('.swiper')[0], {
+      speed: sliderDuration,
+      loop: loopMode,
+      autoHeight: false,
+      centeredSlides: loopMode,
+      followFinger: true,
+      freeMode: false,
+      slideToClickedSlide: false,
+      slidesPerView: 1.5,
+      spaceBetween: 8,
+      rewind: false,
+      mousewheel: {
+        forceToAxis: true
+      },
+      keyboard: false,
+      breakpoints: {
+        // mobile landscape
+        480: {
+          slidesPerView: 2.5,
+          spaceBetween: 8
+        },
+        // tablet
+        768: {
+          slidesPerView: 1.5,
+          spaceBetween: 16
+        },
+        // desktop
+        992: {
+          slidesPerView: 2.5,
+          spaceBetween: 16
+        },
+        1280: {
+          slidesPerView: 2.5,
+          spaceBetween: 16
+        }
+      },
+      pagination: false,
+      // navigation: {
+      //   nextEl: $(this).find(".swiper-next")[0],
+      //   prevEl: $(this).find(".swiper-prev")[0],
+      //   disabledClass: "is-disabled"
+      // },
+      navigation: false,
+      scrollbar: false,
+      slideActiveClass: 'is-active',
+      slideDuplicateActiveClass: 'is-active'
+    });
+  });
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   toggleDrawer();
   aboutAnimation();
@@ -696,4 +1338,11 @@ window.addEventListener('DOMContentLoaded', () => {
   getSydneyTime();
   inViewImageGradientOverlay();
   postImageGridParallax();
+  showVideoAnimation();
+  bgVideoPlayPause();
+  millionQuestions();
+  masterTimeline(); // get better name
+  inspirationHeroCardHover();
+  homeCardCarousel();
+  growingmindsCarousel();
 });
